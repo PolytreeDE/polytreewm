583a91998d6df3275065afccce94eed185848d44
Alex Kotov <kotovalexarian@gmail.com>
2021-11-12 14:31:03 +0500

    Improve movestack patch code

--- config.def.h
+++ config.def.h
@@ -65,7 +65,6 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "gnome-terminal", "--wait", NULL };
 
-#include "movestack.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
