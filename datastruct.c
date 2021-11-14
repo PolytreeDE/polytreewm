11e82dcd1a25ca3b079ff43fbd60071361ae5470
Alex Kotov <kotovalexarian@gmail.com>
2021-11-14 07:43:54 +0500

    Allow to insert into data structure

--- datastruct.c
+++ datastruct.c
@@ -4,7 +4,7 @@
 
 typedef struct Item {
 	struct Item *next;
-	void *value;
+	const void *value;
 } *Item;
 
 struct Datastruct {
@@ -24,5 +24,27 @@ void datastruct_delete(const Datastruct datastruct)
 	// TODO: maybe we should assert?
 	if (datastruct == NULL) return;
 
+	for (Item item = datastruct->top; item; item = item->next) free(item);
 	free(datastruct);
+}
+
+void datastruct_insert_after_value(
+	const Datastruct datastruct,
+	const void *const after_value,
+	const void *const new_value
+) {
+	// TODO: maybe we should assert?
+	if (datastruct == NULL) return;
+
+	Item after_item = datastruct->top;
+	while (after_item && after_item->value != after_value) {
+		after_item = after_item->next;
+	}
+	// TODO: maybe we should assert?
+	if (after_item == NULL) return;
+
+	Item new_item = malloc(sizeof(struct Item));
+	new_item->next = after_item->next;
+	new_item->value = new_value;
+	after_item->next = new_item;
 }
--- datastruct.h
+++ datastruct.h
@@ -13,4 +13,10 @@ typedef struct Datastruct *Datastruct;
 Datastruct datastruct_new();
 void datastruct_delete(Datastruct datastruct);
 
+void datastruct_insert_after_value(
+	Datastruct datastruct,
+	const void *after_value,
+	const void *new_value
+);
+
 #endif // _DATASTRUCT_H
