d5e58baff8ef34eb269c695feec836e6d630c630
Alex Kotov <kotovalexarian@gmail.com>
2021-11-14 06:00:01 +0500

    Improve code of view func

--- dwm.c
+++ dwm.c
@@ -2673,36 +2673,36 @@ updatewmhints(Client *c)
 void
 view(const Arg *arg)
 {
-	int i;
-	unsigned int tmptag;
+	if ((arg->ui & TAGMASK) == selmon->tagset[selmon->seltags]) return;
 
-	if ((arg->ui & TAGMASK) == selmon->tagset[selmon->seltags])
-		return;
 	selmon->seltags ^= 1; /* toggle sel tagset */
+
 	if (arg->ui & TAGMASK) {
 		selmon->tagset[selmon->seltags] = arg->ui & TAGMASK;
 		selmon->pertag->prevtag = selmon->pertag->curtag;
 
-		if (arg->ui == ~0)
+		if (arg->ui == ~0) {
 			selmon->pertag->curtag = 0;
-		else {
-			for (i = 0; !(arg->ui & 1 << i); i++) ;
+		} else {
+			int i = 0;
+			while (!(arg->ui & 1 << i)) ++i;
 			selmon->pertag->curtag = i + 1;
 		}
 	} else {
-		tmptag = selmon->pertag->prevtag;
+		unsigned int tmptag = selmon->pertag->prevtag;
 		selmon->pertag->prevtag = selmon->pertag->curtag;
 		selmon->pertag->curtag = tmptag;
 	}
 
-	selmon->nmaster = selmon->pertag->nmasters[selmon->pertag->curtag];
-	selmon->mfact = selmon->pertag->mfacts[selmon->pertag->curtag];
-	selmon->sellt = selmon->pertag->sellts[selmon->pertag->curtag];
-	selmon->lt[selmon->sellt] = selmon->pertag->ltidxs[selmon->pertag->curtag][selmon->sellt];
-	selmon->lt[selmon->sellt^1] = selmon->pertag->ltidxs[selmon->pertag->curtag][selmon->sellt^1];
+	selmon->nmaster               = selmon->pertag->nmasters[selmon->pertag->curtag];
+	selmon->mfact                 = selmon->pertag->mfacts[selmon->pertag->curtag];
+	selmon->sellt                 = selmon->pertag->sellts[selmon->pertag->curtag];
+	selmon->lt[selmon->sellt]     = selmon->pertag->ltidxs[selmon->pertag->curtag][selmon->sellt];
+	selmon->lt[selmon->sellt ^ 1] = selmon->pertag->ltidxs[selmon->pertag->curtag][selmon->sellt ^ 1];
 
-	if (selmon->showbar != selmon->pertag->showbars[selmon->pertag->curtag])
+	if (selmon->showbar != selmon->pertag->showbars[selmon->pertag->curtag]) {
 		togglebar(NULL);
+	}
 
 	focus(NULL);
 	arrange(selmon);
