9c7ab7bf24c3a32373a05d596c8fb4f8f4dde182
Alex Kotov <kotovalexarian@gmail.com>
2021-11-16 09:19:29 +0500

    Change quit shortcut to Win+Ctrl+Shift+q
    to not quit accidentally

--- config.def.h
+++ config.def.h
@@ -88,7 +88,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {0} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
