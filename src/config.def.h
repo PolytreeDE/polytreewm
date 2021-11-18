52b2b06c1fe897bf579377cccb2667b5b0ec53d7
Alex Kotov <kotovalexarian@gmail.com>
2021-11-18 19:41:36 +0500

    Better organize keyboard shortcuts

--- src/config.def.h
+++ src/config.def.h
@@ -53,42 +53,17 @@ static const Layout layouts[] = {
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
 static Key keys[] = {
-	/* modifier                     key              function        argument */
-	{ MODKEY,                       XK_n,            nametag,        {0} },
-	{ MODKEY,                       XK_z,            spawn,          {.v = "lock" } },
-	{ MODKEY,                       XK_slash,        spawn,          {.v = "menu" } },
-	{ MODKEY|ShiftMask,             XK_slash,        spawn,          {.v = "term" } },
-	{ MODKEY|ShiftMask,             XK_f,            spawn,          {.v = "firefox" } },
-	{ MODKEY,                       XK_b,            togglebar,      {0} },
-	{ MODKEY,                       XK_j,            focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,            focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,            incnmaster,     {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_i,            resetnmaster,   {.i = 1 } },
-	{ MODKEY,                       XK_d,            incnmaster,     {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_d,            resetnmaster,   {.i = 0 } },
-	{ MODKEY,                       XK_h,            setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,            setmfact,       {.f = +0.05} },
-	{ MODKEY|ShiftMask,             XK_j,            movestack,      {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_k,            movestack,      {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_h,            viewrel,        {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_l,            viewrel,        {.i = +1 } },
-	{ MODKEY,                       XK_Return,       zoom,           {0} },
-	{ MODKEY,                       XK_Tab,          view,           {0} },
-	{ MODKEY|ShiftMask,             XK_x,            killclient,     {0} },
-	{ MODKEY,                       XK_m,            setlayout,      {.v = &layouts[0]} }, // Monocle
-	{ MODKEY,                       XK_f,            setlayout,      {.v = &layouts[1]} }, // Floating
-	{ MODKEY,                       XK_t,            setlayout,      {.v = &layouts[2]} }, // Tile
-	{ MODKEY|ShiftMask,             XK_t,            setlayout,      {.v = &layouts[3]} }, // Horizontile
-	{ MODKEY,                       XK_u,            setlayout,      {.v = &layouts[4]} }, // Centeredmaster
-	{ MODKEY,                       XK_space,        setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,        togglefloating, {0} },
-	{ MODKEY,                       XK_0,            view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,            tag,            {.ui = ~0 } },
+	// WM
+	{ MODKEY|ControlMask|ShiftMask, XK_q,            quit,           {0} },
+	// Monitor
 	{ MODKEY,                       XK_bracketleft,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_bracketright, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_bracketleft,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_bracketright, tagmon,         {.i = +1 } },
-	{ MODKEY|ControlMask|ShiftMask, XK_q,            quit,           {0} },
+	// Tag
+	{ MODKEY,                       XK_n,            nametag,        {0} },
+	{ MODKEY,                       XK_0,            view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,            tag,            {.ui = ~0 } },
 	TAGKEYS(                        XK_1,                            0)
 	TAGKEYS(                        XK_2,                            1)
 	TAGKEYS(                        XK_3,                            2)
@@ -98,10 +73,42 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                            6)
 	TAGKEYS(                        XK_8,                            7)
 	TAGKEYS(                        XK_9,                            8)
+	{ MODKEY,                       XK_Tab,          view,           {0} },
+	{ MODKEY|ShiftMask,             XK_h,            viewrel,        {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_l,            viewrel,        {.i = +1 } },
+	// Layout
+	{ MODKEY,                       XK_m,            setlayout,      {.v = &layouts[0]} }, // Monocle
+	{ MODKEY,                       XK_f,            setlayout,      {.v = &layouts[1]} }, // Floating
+	{ MODKEY,                       XK_t,            setlayout,      {.v = &layouts[2]} }, // Tile
+	{ MODKEY|ShiftMask,             XK_t,            setlayout,      {.v = &layouts[3]} }, // Horizontile
+	{ MODKEY,                       XK_u,            setlayout,      {.v = &layouts[4]} }, // Centeredmaster
+	{ MODKEY,                       XK_space,        setlayout,      {0} },
+	{ MODKEY,                       XK_i,            incnmaster,     {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_i,            resetnmaster,   {.i = 1 } },
+	{ MODKEY,                       XK_d,            incnmaster,     {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_d,            resetnmaster,   {.i = 0 } },
+	// Stack
+	{ MODKEY,                       XK_j,            focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,            focusstack,     {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_j,            movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,            movestack,      {.i = -1 } },
+	{ MODKEY,                       XK_Return,       zoom,           {0} },
+	// Window
+	{ MODKEY|ShiftMask,             XK_x,            killclient,     {0} },
+	{ MODKEY|ShiftMask,             XK_space,        togglefloating, {0} },
+	// Appearance
+	{ MODKEY,                       XK_b,            togglebar,      {0} },
+	{ MODKEY,                       XK_h,            setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,            setmfact,       {.f = +0.05} },
 	{ MODKEY|Mod1Mask,              XK_b,            configborder,   {.i = -1 } },
 	{ MODKEY|Mod1Mask|ShiftMask,    XK_b,            configborder,   {.i = +1 } },
 	{ MODKEY|Mod1Mask,              XK_g,            configgap,      {.i = -1 } },
 	{ MODKEY|Mod1Mask|ShiftMask,    XK_g,            configgap,      {.i = +1 } },
+	// Starting applications
+	{ MODKEY,                       XK_z,            spawn,          {.v = "lock" } },
+	{ MODKEY,                       XK_slash,        spawn,          {.v = "menu" } },
+	{ MODKEY|ShiftMask,             XK_slash,        spawn,          {.v = "term" } },
+	{ MODKEY|ShiftMask,             XK_f,            spawn,          {.v = "firefox" } },
 };
 
 /* button definitions */
