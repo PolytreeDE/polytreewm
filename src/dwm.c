6eab90a11514a77cee625d3d4c02e25f5fee3a4f
Alex Kotov <kotovalexarian@gmail.com>
2021-11-20 20:46:40 +0500

    Improve code style of macros in "src/dwm.c"

--- src/dwm.c
+++ src/dwm.c
@@ -40,15 +40,24 @@
  * macros *
  **********/
 
-#define BUTTONMASK              (ButtonPressMask|ButtonReleaseMask)
-#define CLEANMASK(mask)         (mask & ~(numlockmask|LockMask) & (ShiftMask|ControlMask|Mod1Mask|Mod2Mask|Mod3Mask|Mod4Mask|Mod5Mask))
-#define INTERSECT(x,y,w,h,m)    (MAX(0, MIN((x)+(w),(m)->wx+(m)->ww) - MAX((x),(m)->wx)) \
-                               * MAX(0, MIN((y)+(h),(m)->wy+(m)->wh) - MAX((y),(m)->wy)))
-#define ISVISIBLE(C)            (true)
-#define LENGTH(X)               (sizeof X / sizeof X[0])
-#define MOUSEMASK               (BUTTONMASK|PointerMotionMask)
-#define WIDTH(X)                ((X)->w + 2 * (X)->bw)
-#define HEIGHT(X)               ((X)->h + 2 * (X)->bw)
+#define BUTTONMASK   (ButtonPressMask | ButtonReleaseMask)
+#define ISVISIBLE(C) (true)
+#define LENGTH(X)    (sizeof(X) / sizeof(X[0]))
+#define MOUSEMASK    (BUTTONMASK | PointerMotionMask)
+#define WIDTH(X)     ((X)->w + 2 * (X)->bw)
+#define HEIGHT(X)    ((X)->h + 2 * (X)->bw)
+
+#define CLEANMASK(mask) (                            \
+	(mask) &                                         \
+	~(numlockmask | LockMask) &                      \
+	(ShiftMask | ControlMask | Mod1Mask | Mod2Mask | \
+		Mod3Mask | Mod4Mask | Mod5Mask)              \
+)
+
+#define INTERSECT(x,y,w,h,m) (                                      \
+	MAX(0, MIN((x) + (w), (m)->wx + (m)->ww) - MAX((x), (m)->wx)) * \
+	MAX(0, MIN((y) + (h), (m)->wy + (m)->wh) - MAX((y), (m)->wy))   \
+)
 
 /*********
  * types *
