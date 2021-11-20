84ba2de1b86a0e16034456591a4e90b86253d700
Alex Kotov <kotovalexarian@gmail.com>
2021-11-20 18:53:31 +0500

    Add empty line

--- src/dwm.c
+++ src/dwm.c
@@ -41,6 +41,7 @@
 /**********
  * macros *
  **********/
+
 #define BUTTONMASK              (ButtonPressMask|ButtonReleaseMask)
 #define CLEANMASK(mask)         (mask & ~(numlockmask|LockMask) & (ShiftMask|ControlMask|Mod1Mask|Mod2Mask|Mod3Mask|Mod4Mask|Mod5Mask))
 #define INTERSECT(x,y,w,h,m)    (MAX(0, MIN((x)+(w),(m)->wx+(m)->ww) - MAX((x),(m)->wx)) \
