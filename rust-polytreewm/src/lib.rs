b88b936ccf15f84a291dcf164e313d8d4c2c9049
Alex Kotov <kotovalexarian@gmail.com>
2022-09-09 19:05:39 +0400

    Revert "Remove Xbase in Rust"
    This reverts commit 333b3163d3e66407cf75a61ec8e2ce9149b0e422.

--- rust-polytreewm/src/lib.rs
+++ rust-polytreewm/src/lib.rs
@@ -1,6 +1,7 @@
 mod api;
 mod atoms;
 mod constraints;
+mod xbase;
 
 pub mod geom;
 pub mod settings;
@@ -8,3 +9,4 @@ pub mod unit;
 
 pub use atoms::Atoms;
 pub use settings::Settings;
+pub use xbase::Xbase;
