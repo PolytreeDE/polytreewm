4dc0c65125debf9fc3fd20524b49a9333ade6fb3
Alex Kotov <kotovalexarian@gmail.com>
2022-09-09 17:35:44 +0400

    Specify X11 atoms

--- rust-winproto/src/lib.rs
+++ rust-winproto/src/lib.rs
@@ -1,1 +1,22 @@
-pub fn foo() {}
+use x11rb::atom_manager;
+
+atom_manager! {
+    pub AtomCollection: AtomCollectionCookie {
+        WM_PROTOCOLS,
+        WM_DELETE_WINDOW,
+        WM_STATE,
+        WM_TAKE_FOCUS,
+
+        _NET_ACTIVE_WINDOW,
+        _NET_SUPPORTED,
+        _NET_WM_NAME,
+        _NET_WM_STATE,
+        _NET_SUPPORTING_WM_CHECK,
+        _NET_WM_STATE_FULLSCREEN,
+        _NET_WM_WINDOW_TYPE,
+        _NET_WM_WINDOW_TYPE_DIALOG,
+        _NET_CLIENT_LIST,
+
+        UTF8_STRING,
+    }
+}
